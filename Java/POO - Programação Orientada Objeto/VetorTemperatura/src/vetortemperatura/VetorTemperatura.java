6d66d4a2b693537b5c0cac84896f443f37ff2ae9
Daniel Garcêz <Daniel_gzribeiro@outlook.com>
2024-04-11 17:37:35 -0300

    VetorTemperatura-02
    Fiz usando JOptionPane.

--- Java/POO - Programação Orientada Objeto/VetorTemperatura/src/vetortemperatura/VetorTemperatura.java
+++ Java/POO - Programação Orientada Objeto/VetorTemperatura/src/vetortemperatura/VetorTemperatura.java
@@ -1,6 +1,7 @@
 package vetortemperatura;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class VetorTemperatura {
 
@@ -8,12 +9,14 @@ public class VetorTemperatura {
         int Temperatura[] = new int[7];
         int Soma, Media, i, Acima, Abaixo;
         Soma =0;
-        Scanner dados = new Scanner(System.in);
         
         for(i=0; i<7; i++){
-            System.out.println("Digite a "+(i+1)+"°temperatura");
+            Scanner dados = new Scanner(System.in);
+            String num = JOptionPane.showInputDialog("Digite a "+(i+1)+"° temperatura:");
+            //System.out.println("Digite a "+(i+1)+"°temperatura");
+            //Temperatura[i] = dados.nextInt();
+            Temperatura[i] = Integer.parseInt(num);
             Soma= Soma + Temperatura[i];
-            Temperatura[i] = dados.nextInt();
         }
         Media = Soma /7;
         Acima = 0;
@@ -26,9 +29,12 @@ public class VetorTemperatura {
                Abaixo++;
            }
         }
-        System.out.println("A média de temperaturas é "+Media);
-        System.out.println("Total de dias que a temperatura ficou ACIMA da média: "+Acima);
-        System.out.println("Total de dias que a temperatura ficou ABAIXO da média: "+Abaixo);
+        JOptionPane.showMessageDialog(null,"A média de temperaturas é "+Media);
+        //System.out.println("A média de temperaturas é "+Media);
+        JOptionPane.showMessageDialog(null,"Total de dias que a temperatura ficou ACIMA da média: "+Acima);
+        //System.out.println("Total de dias que a temperatura ficou ACIMA da média: "+Acima);
+        JOptionPane.showMessageDialog(null,"Total de dias que a temperatura ficou ABAIXO da média: "+Abaixo);
+        //System.out.println("Total de dias que a temperatura ficou ABAIXO da média: "+Abaixo);
     }
     
 }

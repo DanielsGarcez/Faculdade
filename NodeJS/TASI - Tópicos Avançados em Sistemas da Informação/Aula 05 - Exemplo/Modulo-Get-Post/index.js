17b97b5ca2a79e72269f6db8fabd002f3ff4ae7f
DanielsGarcez <137909210+DanielsGarcez@users.noreply.github.com>
2024-04-12 20:27:30 -0300

    Aula 05.1 - TAD

--- NodeJS/TASI - Tópicos Avançados em Sistemas da Informação/Aula 05 - Exemplo/Modulo-Get-Post/index.js
+++ NodeJS/TASI - Tópicos Avançados em Sistemas da Informação/Aula 05 - Exemplo/Modulo-Get-Post/index.js
@@ -4,6 +4,18 @@ const port = 8081
 const path = require('path');
 const basePath = path.join(__dirname,'html')
 
+var checkAuth =function (req, res, next){
+    req.authStatus = false;
+    if(req.authStatus){
+    console.log('Está logado, pode continuar');
+    next()
+    }else{
+        console.log('Não está logado, faça o login para continuar!');
+    }
+}
+app.use(checkAuth);
+
+
 app.get('/', (req, res)=>{
     res.sendFile(`${basePath}/index.html`)
 })
